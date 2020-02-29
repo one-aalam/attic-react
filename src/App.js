88faa9693fdea241a5032db45b9aa989e4d45f4d
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-02-29 23:05:54 +0530

    Add React router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Home from './Home';
 import SignIn from './SignIn';
 
-function App() {
+import './App.css';
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <SignIn />
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="App">
+    <Router>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+          </ul>
+      </nav>
+      <Switch>
+        <Route path="/signin">
+          <SignIn/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+)
 
 export default App;
