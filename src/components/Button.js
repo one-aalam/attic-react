5a99112790b41dec2bb443a665193da989928cf9
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-03-02 01:00:04 +0530

    Add a Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,66 @@ import React from "react";
 
 export default function Button({ children, ...buttonProps }) {
   return (
-    <button
-      className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
-      {...buttonProps}
-    >
-      {children}
-    </button>
+    <span className="shadow-sm rounded-md">
+      <button
+        className="font-display font-extrabold inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+        {...buttonProps}
+      >
+        {children}
+      </button>
+    </span>
+  );
+}
+
+export function ButtonSecondary({ children, ...buttonProps }) {
+  return (
+    <span className="sm:ml-3 shadow-sm rounded-md">
+      <button
+        className="font-display font-extrabold inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+        {...buttonProps}
+      >
+        {children}
+      </button>
+    </span>
+  );
+}
+
+
+export function ButtonMedium({ children, ...buttonProps }) {
+  return (
+    <span className="shadow rounded-md">
+      <button
+        className="font-display font-extrabold inline-flex items-center justify-center px-5 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-3 md:text-lg md:px-8"
+        {...buttonProps}
+      >
+        {children}
+      </button>
+    </span>
+  );
+}
+
+export function ButtonLarge({ children, ...buttonProps }) {
+  return (
+    <span className="shadow rounded-md">
+      <button
+        className="font-display font-extrabold inline-flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+        {...buttonProps}
+      >
+        {children}
+      </button>
+    </span>
+  );
+}
+
+export function ButtonLargeSecondary({ children, ...buttonProps }) {
+  return (
+    <span className="shadow rounded-md">
+      <button
+        className="font-display font-extrabold inline-flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+        {...buttonProps}
+      >
+        {children}
+      </button>
+    </span>
   );
 }
