885039470abac482f8d6ee6d2313554b909ad64d
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-03-01 21:08:10 +0530

    Remove index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './fonts/Inter/inter.css';
 import './fonts/Gilroy/gilroy.css';
-import './index.css';
 import './styles.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
