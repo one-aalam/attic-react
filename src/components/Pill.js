e1ed56df44e990d060d58d1892e32d4a81a06986
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-03-02 01:00:40 +0530

    Add a pill component

---
+++ src/components/Pill.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Pill = ({ children }) => (
+    <span className="flex-shrink-0 ml-3 flex items-center rounded-full px-3 py-1 bg-gray-100">{children}</span>
+)
+
+export default Pill;
